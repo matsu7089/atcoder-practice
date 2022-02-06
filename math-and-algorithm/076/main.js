013a1da7edd7f6718966b8e98492a84cecea7483
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2022-02-06 12:28:28 +0900

    update: math-and-algorithm 076

--- math-and-algorithm/076/main.js
+++ math-and-algorithm/076/main.js
@@ -7,15 +7,9 @@ const A = input[1]
   .sort((a, b) => a - b)
   .map((v) => BigInt(v))
 
-const sum = Array(N)
-sum[N - 1] = A[N - 1]
-for (let i = N - 2; i >= 0; i--) {
-  sum[i] = sum[i + 1] + A[i]
-}
-
 let ans = 0n
-for (let i = 0; i < N - 1; i++) {
-  ans += sum[i + 1] - A[i] * BigInt(N - i - 1)
+for (let i = 0; i < N; i++) {
+  ans += A[i] * BigInt(2 * i - N + 1)
 }
 
 console.log(ans.toString())
